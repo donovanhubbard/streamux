656104cbba3e5c693499edab3ee2cad110d0de07
Donovan Hubbard <37090676+donovanhubbard@users.noreply.github.com>
2026-05-13 22:02:44 -0700

    Including makefile

--- streamux-gate/install-streamux-gate.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rm /Users/tmuxview/streamux-gate
-cp streamux-gate /Users/tmuxview/
-chown tmuxview:staff /Users/tmuxview/streamux-gate
-chmod 744 /Users/tmuxview/streamux-gate
